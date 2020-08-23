e759e1b998f815a46fdab75f4a292e2d42682d13
Glitch (separate-cuddly-coconut) <none>
2020-08-23 22:00:17 +0000

    🐙🎉 Checkpoint ./service-worker.js:21418923/3717

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,5 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-/*const {strategies} = workbox;
-
-self.addEventListener('fetch', (event) => {
-  if (event.request.url.endsWith('/schedules/')) {
-    // Oops! This causes workbox-strategies.js to be imported inside a fetch handler,
-    // outside of the initial, synchronous service worker execution.
-    const cacheFirst = new workbox.strategies.CacheFirst();
-    event.respondWith(cacheFirst.handle({request: event.request}));
-  }
-});*/
 if (workbox) {
   console.log(`Yay! Workbox is loaded 🎉`);
 
@@ -17,34 +7,8 @@ if (workbox) {
   console.log(`Boo! Workbox didn't load 😬`);
 }
 
-workbox.routing.registerRoute(
-   ({request}) => request.destination === 'script',
-  new workbox.strategies.CacheFirst({
-    cacheName: 'static-files',
-    plugins: [
-      new workbox.expiration.ExpirationPlugin({
-        maxEntries: 50,
-        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-      })
-    ]
-  })
-);
-
-workbox.routing.registerRoute(
-   ({request}) => request.destination === 'image',
-  new workbox.strategies.CacheFirst({
-    cacheName: 'static-files',
-    plugins: [
-      new workbox.expiration.ExpirationPlugin({
-        maxEntries: 50,
-        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-      })
-    ]
-  })
-);
-
 /*workbox.routing.registerRoute(
-   ({request}) => request.destination === 'style',
+   ({request}) => request.destination === 'script',
   new workbox.strategies.CacheFirst({
     cacheName: 'static-files',
     plugins: [
@@ -58,13 +22,26 @@ workbox.routing.registerRoute(
 
 const {strategies} = workbox;
 
-self.addEventListener('fetch', (event) => {
-  console.log("entreee");
-  if (event.request.url.endsWith('.css')) {
+self.addEventListener('fetch', (evt) => {
+  /*if (event.request.url.endsWith('.css')) {
     console.log("entree22");
     // Using the previously-initialized strategies will work as expected.
     const cacheFirst = new strategies.CacheFirst({cacheName: 'static-files'});
     cacheFirst.handle({request: event.request}).then(data=> console.log(data.body));
     event.respondWith(cacheFirst.handle({request: event.request}));
+  }*/
+  
+  if (evt.request.url.includes('/schedules/')) {
+    console.log('[Service Worker] Fetch (data) from url /schedules/', evt.request.url);
+
+    const cacheFirst = new strategies.CacheFirst({cacheName: 'data-cache'});
+    cacheFirst.handle({request: evt.request});
+    evt.respondWith(cacheFirst.handle({request: evt.request}));
+  }
+  else{
+    console.log('[Service Worker] Fetch (static-data)', evt.request.url);
+    const cacheFirst = new strategies.CacheFirst({cacheName: 'static-files'});
+    cacheFirst.handle({request: evt.request})
+    evt.respondWith(cacheFirst.handle({request: evt.request}));
   }
 });
