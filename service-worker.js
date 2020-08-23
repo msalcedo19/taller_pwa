8fd0598c037431951ea6ce4e85adf578585fa0be
Glitch (separate-cuddly-coconut) <none>
2020-08-23 17:14:04 +0000

    ☕️👋 Checkpoint ./service-worker.js:21418923/2110 ./index.html:21418923/702

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,20 @@
-import {precaching} from 'workbox-precaching';
-import {registerRoute} from 'workbox-routing';
-import {BackgroundSyncPlugin} from 'workbox-background-sync';
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+/*const {strategies} = workbox;
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.url.endsWith('/schedules/')) {
+    // Oops! This causes workbox-strategies.js to be imported inside a fetch handler,
+    // outside of the initial, synchronous service worker execution.
+    const cacheFirst = new workbox.strategies.CacheFirst();
+    event.respondWith(cacheFirst.handle({request: event.request}));
+  }
+});*/
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+
+  workbox.precaching.precacheAndRoute([]);
+
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
